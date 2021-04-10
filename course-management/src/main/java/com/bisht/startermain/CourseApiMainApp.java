63db4c62a8f1e34cc796a776dc6fd2da2c175912
Nitin Bisht <3498georgian@gmail.com>
2021-04-10 12:10:54 +0530

    Added main class with annotation @SpringBootApplication.

---
+++ course-management/src/main/java/com/bisht/startermain/CourseApiMainApp.java
@@ -0,0 +1,12 @@
+package com.bisht.startermain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApiMainApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseApiMainApp.class, args);
+    }
+}
