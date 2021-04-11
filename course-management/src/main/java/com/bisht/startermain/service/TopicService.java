8ed6692636d3df464996af19ae1b8108105e643e
Nitin Bisht <3498georgian@gmail.com>
2021-04-11 19:21:38 +0530

    Implemented Update and Delete Request.

--- course-management/src/main/java/com/bisht/startermain/service/TopicService.java
+++ course-management/src/main/java/com/bisht/startermain/service/TopicService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class TopicService {
@@ -26,5 +27,27 @@ public class TopicService {
 
     public void addTopic(Topic topic) {
         topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic updatedTopic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic topic = topics.get(i);
+            if (topic.getId().equals(id)) {
+                topics.set(i, updatedTopic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(new Predicate<Topic>() {
+            @Override
+            public boolean test(Topic topic) {
+                if (topic.getId().equals(id)) {
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
